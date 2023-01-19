f096582ddaf90002385fc75bdffdc193e84c892c
Bora <bora@Bora-MacBook-Air.local>
2023-01-19 16:31:34 +0300

    finished basket page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,10 +2,15 @@ import Header from '../components/Header';
 import CheckoutProduct from '../components/CheckoutProduct';
 import Image from 'next/image';
 import { useSelector } from 'react-redux';
-import { selectItems } from '../slices/basketSlice';
+import { selectItems, selectTotal } from '../slices/basketSlice';
+import { useSession } from 'next-auth/react';
+import Currency from 'react-currency-formatter';
 
 function Checkout() {
+  const { data: session } = useSession();
   const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+
   return (
     <div className='bg-gray-100'>
       <Header />
@@ -40,6 +45,29 @@ function Checkout() {
         </div>
 
         {/* Right */}
+        <div className='flex flex-col bg-white p-10 shadow-md'>
+          {items.length > 0 && (
+            <>
+              <h2 className='whitespace-nowrap'>
+                Subtotal ({items.length} items):{' '}
+                <Currency
+                  className='font-bold'
+                  currency='TRY'
+                  quantity={total}
+                />
+              </h2>
+              <button
+                disabled={!session}
+                className={`button mt-2 ${
+                  !items.length &&
+                  'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'
+                }`}
+              >
+                {!session ? 'Sign in to checkout' : 'Proceed to checkout'}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
